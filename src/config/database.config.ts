881ac21269558b2171ae7e062aece0c7df722724
StevanPataki99 <stevan.pataki.1999@gmail.com>
2023-02-11 18:53:00 +0100

    fix: db config vars

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 const host = process.env.DATABASE_HOST || 'localhost';
 const port = parseInt(process.env.DATABASE_PORT || '5432', 10);
-const username = process.env.DATABASE_USERNAME || 'test123';
-const password = process.env.DATABASE_PASSWORD || 'pguser';
+const username = process.env.DATABASE_USERNAME || 'pguser';
+const password = process.env.DATABASE_PASSWORD || 'test123';
 const database = process.env.DATABASE_NAME || 'dice-betting';
 
 export default {
